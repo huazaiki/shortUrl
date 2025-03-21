1d2b7841a02c6936871f4c9ebd46227480214f87
huazaiki <huazaiki@gmail.com>
2025-03-21 16:14:23 +0800

    init: 初始化项目

---
+++ src/test/java/com/huazaiki/shorturl/ShortUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huazaiki.shorturl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
